c0746758385da12c74129c80aa0b93bc661abae2
ManarMahmalji <31004981+ManarMahmalji@users.noreply.github.com>
2017-08-20 15:33:23 +0300

    Finished the interface

--- TIC TAC TOE/TIC TAC TOE/ViewController.swift
+++ TIC TAC TOE/TIC TAC TOE/ViewController.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+@IBOutlet weak var statusLabel: UILabel!
+@IBOutlet weak var newGameButton: UIButton!
+@IBOutlet weak var button1: UIButton!
+@IBOutlet weak var button2: UIButton!
+@IBOutlet weak var button3: UIButton!
+@IBOutlet weak var button4: UIButton!
+@IBOutlet weak var button5: UIButton!
+@IBOutlet weak var button6: UIButton!
+@IBOutlet weak var button7: UIButton!
+@IBOutlet weak var button8: UIButton!
+@IBOutlet weak var button9: UIButton!
+    
+    
+    @IBAction func newGameButtonClicked(_ sender: AnyObject) {
+    }
+    
+    
+    @IBAction func buttonClicked(_ sender: AnyObject) {
+    }
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
