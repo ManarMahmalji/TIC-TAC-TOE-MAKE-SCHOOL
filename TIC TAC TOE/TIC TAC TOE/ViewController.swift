adac141b5a942057f05d1c952dbaaa88fb940a83
ManarMahmalji <31004981+ManarMahmalji@users.noreply.github.com>
2017-08-20 14:50:46 +0300

    Initial Commit

---
+++ TIC TAC TOE/TIC TAC TOE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIC TAC TOE
+//
+//  Created by Manar Mahmalji on 8/20/17.
+//  Copyright © 2017 Manar Mahmalji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
